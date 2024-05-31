8727fa3fd6ad44d4e7f2f48fd1c71e4acd4bb39e
Md. Aminul Islam Shaon <aminul007.ais@gmail.com>
2024-05-31 12:57:17 +0600

    time to sleep

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from "react-router-dom";
 import './index.css'
+import router from './Routes/Routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
